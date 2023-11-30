27acdcce844def9cf6e64d10494989b6c2465a88
Paulo André C. Carvalho <paulocarvalho2v@gmail.com>
2023-11-30 17:08:28 -0300

    Atualização do código index.js do scraper (agora está funcionando como deveria)

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -8,22 +8,83 @@ module.exports.handler = async () => {
             userDataDir: "./tmp"
         });
         const page = await browser.newPage();
+        await page.setViewport({
+            width: 1520,
+            height: 1140,
+        });
         await page.goto('https://www.amazon.com.br/gp/bestsellers/?ref_=nav_cs_bestsellers');
-    
+
         // await page.waitForNavigation();
-    
-        // const productsHandles = await page.$$('div#anonCarousel1');
-        const productsHandles = await page.$$('.a-carousel-card');
-        await page.screenshot({
-            path: 'screenshot.jpg'
-          });
-        console.log(productsHandles);
-        for(const producthandle of productsHandles){
-            const title = await page.evaluate(el => el.querySelector("span > div").textContent, producthandle)
-            
-    
-            console.log(title)
+
+        const categoriesHandles = await page.$$('div.a-row.a-carousel-header-row.a-size-large'); // div > div > div > div.a-row.a-carousel-header-row.a-size-large
+
+        let categories = [];
+
+        for (const categoryhandle of categoriesHandles) {
+
+            let category = "Null"
+
+            try{
+                category = await page.evaluate(el => el.querySelector('.a-carousel-heading').textContent, categoryhandle)
+            } catch (error) {}
+
+            if (category !== "Null") {
+                categories.push(category);
+            }
         }
+
+        const productsHandles = await page.$$('.a-carousel-card');
+
+        let items = [];
+
+        for (const producthandle of productsHandles) {
+
+            let title = "Null"
+
+            try {
+                title = await page.evaluate(el => el.querySelector("span > div").textContent, producthandle)
+            } catch (error) { }
+
+            if (title !== "Null") {
+                items.push(title);
+            }
+        }
+
+        const divideArray = (arr, groupSizes, skipCount) => {
+            const result = [];
+            let currentIndex = 0;
+
+            for (const groupSize of groupSizes) {
+                const currentGroup = [];
+                for (let i = 0; i < groupSize; i++) {
+                    if (currentIndex < arr.length) {
+                        currentGroup.push(arr[currentIndex]);
+                        currentIndex++;
+                    }
+                }
+                result.push(currentGroup);
+                currentIndex += skipCount; // Pula os valores após formar um grupo
+            }
+
+            return result;
+        };
+
+        // Dividindo os 12 primeiros valores em grupos de 3, pulando 1 valor entre os grupos
+        const firstGroups = divideArray(items.slice(0, 12), [3, 3, 3], 1);
+
+        // Dividindo os 18 valores restantes em grupos de 3, pulando 3 valores entre os grupos
+        const remainingGroups = divideArray(items.slice(12), [3, 3, 3, 3, 3, 3], 3);
+
+        // Juntando os resultados
+        const finalResult = [...firstGroups, ...remainingGroups.filter(group => group.length > 0)];
+
+        const groupedCategories = categories.slice(0, finalResult.length).map((category, index) => ({
+            category,
+            items: finalResult[index],
+        }));
+        
+        console.log(groupedCategories);
+
         // await browser.close();
 
         return {
