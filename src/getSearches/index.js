07352f74ddc62c59aa12bde75027b374cc907871
Paulo André C. Carvalho <paulocarvalho2v@gmail.com>
2023-11-30 22:09:39 -0300

    Atualização do index.js do scraper para mostrar informações a mais (porém sem inserir na tabela do dynamoDB)

--- src/getSearches/index.js
+++ src/getSearches/index.js
@@ -2,7 +2,7 @@ const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
 
 const dynamoDBClient = new DynamoDBClient({ region: 'us-east-1' });
 
-module.exports.handler = async (event) => {
+module.exports.handler = async () => {
   try {
     const params = {
       TableName: "amazon-products",
--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,11 +1,111 @@
 const puppeteer = require('puppeteer');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+// import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
+const dynamoDBClient = new DynamoDBClient({ region: 'us-east-1' });
+
+// Função para juntar as categorias com os produtos
+const combineArrays = (arrayA, arrayB) => {
+    const arrayC = [];
+
+    for (let i = 0; i < arrayA.length; i++) {
+        const category = arrayA[i];
+        const items = arrayB.slice(i * 3, (i + 1) * 3);
+
+        const combinedObject = {
+            category,
+            items,
+        };
+
+        arrayC.push(combinedObject);
+    }
+
+    return arrayC;
+};
+
+// Função para extrair produtos e ranks da página
+const extractProducts = async (page) => {
+
+    let products = [];
+    let categories = [];
+
+    const categoriesHandles = await page.$$('div.a-row.a-carousel-header-row.a-size-large');
+
+    for (const categoryhandle of categoriesHandles) {
+        let category = "Null";
+
+        try {
+            category = await page.evaluate(el => el.querySelector('.a-carousel-heading').textContent, categoryhandle);
+        } catch (error) { }
+
+        if (category !== "Null") {
+            categories.push(category);
+        }
+    }
+
+    const productsHandles = await page.$$('.a-carousel-card');
+
+    for (const producthandle of productsHandles) {
+        let title = "Null";
+        let rank = "Null";
+        let image = "Null";
+        let avaliation = "Null";
+        let price = "Null";
+
+        try {
+            title = await page.evaluate(el => el.querySelector("span > div").textContent, producthandle);
+        } catch (error) { }
+
+        try {
+            rank = await page.evaluate(el => el.querySelector("div > div.a-section.zg-bdg-ctr > div.a-section.zg-bdg-body.zg-bdg-clr-body.aok-float-left > span").textContent, producthandle);
+        } catch (error) { }
+
+        try {
+            image = await page.evaluate(el => el.querySelector("div > img").getAttribute("src"), producthandle)
+        } catch (error) { }
+
+        try {
+            avaliation = await page.evaluate(el => el.querySelector("div > a > i > span").textContent, producthandle)
+        } catch (error) { }
+
+        try {
+            price = await page.evaluate(el => el.querySelector("div > div > a > div > span > span").textContent, producthandle)
+        } catch (error) { }
+
+        if (title !== "Null" && rank !== "Null" && (rank === "#1" || rank === "#2" || rank === "#3")) {
+            let product = rank + ": " + title;
+            products.push({ product, image, avaliation, price });
+        }
+    }
+
+    const groupedByCategory = combineArrays(categories, products);
+
+    return groupedByCategory;
+};
+
+/*
+// Função para inserir dados no DynamoDB
+const insertDataIntoDynamoDB = async (groupedCategories) => {
+    await Promise.all(groupedCategories.map(async (group) => {
+        const params = {
+            TableName: 'amazon-products',
+            Item: marshall({
+                category: group.category,
+                items: group.items,
+            }),
+        };
+
+        const putCommand = new PutItemCommand(params);
+        await dynamoDBClient.send(putCommand);
+    }));
+};
+*/
 module.exports.handler = async () => {
     try {
         const browser = await puppeteer.launch({
             headless: false,
             defaultViewport: false,
-            userDataDir: "./tmp"
+            userDataDir: "./tmp",
         });
         const page = await browser.newPage();
         await page.setViewport({
@@ -14,78 +114,14 @@ module.exports.handler = async () => {
         });
         await page.goto('https://www.amazon.com.br/gp/bestsellers/?ref_=nav_cs_bestsellers');
 
-        // await page.waitForNavigation();
+        const test = await extractProducts(page);
 
-        const categoriesHandles = await page.$$('div.a-row.a-carousel-header-row.a-size-large'); // div > div > div > div.a-row.a-carousel-header-row.a-size-large
-
-        let categories = [];
-
-        for (const categoryhandle of categoriesHandles) {
-
-            let category = "Null"
-
-            try{
-                category = await page.evaluate(el => el.querySelector('.a-carousel-heading').textContent, categoryhandle)
-            } catch (error) {}
-
-            if (category !== "Null") {
-                categories.push(category);
-            }
-        }
-
-        const productsHandles = await page.$$('.a-carousel-card');
-
-        let items = [];
-
-        for (const producthandle of productsHandles) {
-
-            let title = "Null"
-
-            try {
-                title = await page.evaluate(el => el.querySelector("span > div").textContent, producthandle)
-            } catch (error) { }
-
-            if (title !== "Null") {
-                items.push(title);
-            }
-        }
-
-        const divideArray = (arr, groupSizes, skipCount) => {
-            const result = [];
-            let currentIndex = 0;
-
-            for (const groupSize of groupSizes) {
-                const currentGroup = [];
-                for (let i = 0; i < groupSize; i++) {
-                    if (currentIndex < arr.length) {
-                        currentGroup.push(arr[currentIndex]);
-                        currentIndex++;
-                    }
-                }
-                result.push(currentGroup);
-                currentIndex += skipCount; // Pula os valores após formar um grupo
-            }
-
-            return result;
-        };
-
-        // Dividindo os 12 primeiros valores em grupos de 3, pulando 1 valor entre os grupos
-        const firstGroups = divideArray(items.slice(0, 12), [3, 3, 3], 1);
-
-        // Dividindo os 18 valores restantes em grupos de 3, pulando 3 valores entre os grupos
-        const remainingGroups = divideArray(items.slice(12), [3, 3, 3, 3, 3, 3], 3);
-
-        // Juntando os resultados
-        const finalResult = [...firstGroups, ...remainingGroups.filter(group => group.length > 0)];
-
-        const groupedCategories = categories.slice(0, finalResult.length).map((category, index) => ({
-            category,
-            items: finalResult[index],
-        }));
-        
-        console.log(groupedCategories);
+        // console.log(test);
+        // await insertDataIntoDynamoDB(groupedCategories);
 
         // await browser.close();
+
+        console.log(JSON.stringify(test, null, 2));
 
         return {
             statusCode: 200,
